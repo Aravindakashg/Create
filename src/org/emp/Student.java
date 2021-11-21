1370ddbe8f445955bfa28bdd8152a832731070ae
Aravindakashg <ak5785340@gmail.com>
2021-11-21 20:40:17 +0530

    Done

---
+++ src/org/emp/Student.java
@@ -0,0 +1,13 @@
+package org.emp;
+
+public class Student {
+	private void add() {
+		System.out.println("add");
+
+	}
+	public static void main(String[] args) {
+		Student a=new Student();
+		a.add();
+	}
+
+}
